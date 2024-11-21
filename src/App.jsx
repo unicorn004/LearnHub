e7cad5fb8bf8a66ee5c8525a5f2d2ab3324ee7f7
Tanmay <tanmaysinkar2021@gmail.com>
2024-11-21 20:44:33 +0530

    css fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,18 @@ import Auth from './pages/Auth'; // Import your Auth component
 import ChatApp from './pages/Chats/chat';
 import DiscussionForum from './pages/forum/forum';
 import MultiStepForm from './pages/matchmaking/form';
+
 function App() {
   return (
     <Router>
-      <div className="App min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
-          <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
-            Welcome to EdTech App
-          </h1>
-          <Routes>
-            <Route path="/" element={<Auth />} /> {/* Render Auth page */}
-            <Route path="/chat" element={<ChatApp/>} /> {/* Render Auth page */}
-            {/* You can add more routes here for different pages */}
-            <Route path="/forum" element={<DiscussionForum/>} />
-            <Route path="/match-making-form" element={<MultiStepForm/>} />
-
-          </Routes>
-        </div>
+      <div className="App min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<Auth />} /> {/* Render Auth page */}
+          <Route path="/chat" element={<ChatApp />} /> {/* Render Chat page */}
+          {/* You can add more routes here for different pages */}
+          <Route path="/forum" element={<DiscussionForum />} />
+          <Route path="/match-making-form" element={<MultiStepForm />} />
+        </Routes>
       </div>
     </Router>
   );
